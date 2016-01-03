af52c8fac4e07dca3bd30b88b8a2cdfc7a174c16
Howard Liu <fsgmhoward@gmail.com>
2016-01-03 20:47:00 +0800

    Minor fixes

--- ixlab_adminer/adminauth.php
+++ ixlab_adminer/adminauth.php
@@ -1,6 +1,6 @@
 <?php
-//Written by Howard Liu on Jan 3, 2016
-//Copyright (c) Howard Liu, 2016. All rights reserved
+//Written by FSGM-Howard on Jan 3, 2016
+//Copyright (c) FSGM-Howard, 2016. All rights reserved
 //For more information about software license, please refer to ../LICENSE
 //This file used WMZZ's Library
 
@@ -21,7 +21,7 @@ if (file_exists('../../config.php')){
 
     $conn = new wmysql(DB_HOST,DB_USER,DB_PASSWD,DB_NAME);
 
-    $system_url = $conn->fetch_array($conn->query('SELECT * FROM `'.DB_PREFIX.'options` WHERE `name`="system_url";'));
+    $system_url = $conn->once_fetch_array('SELECT * FROM `'.DB_PREFIX.'options` WHERE `name`="system_url";');
     define('SYSTEM_URL',$system_url['value']);
 
     if (isset($_GET['logout'])){
@@ -33,11 +33,10 @@ if (file_exists('../../config.php')){
     }
 
     $auth = false;
-    while ($row = $conn->fetch_array($conn->query("SELECT * FROM `".DB_PREFIX."ixlab_adminer` WHERE `token`='".$_COOKIE['adminer_token']."';"))){
-        if ($row['expire_time']>=time()){
-            $conn->query('UPDATE '.DB_PREFIX.'ixlab_adminer SET expire_time='.(time()+600).' WHERE token="'.$_COOKIE['adminer_token'].'"');
-            setcookie('adminer_token', $_COOKIE['adminer_token'], time()+600, '/');
-            $auth = true;
-        }
+    $row = $conn->once_fetch_array("SELECT * FROM `".DB_PREFIX."ixlab_adminer` WHERE `token`='".$_COOKIE['adminer_token']."';");
+    if ($row['expire_time']>=time()){
+        $conn->query('UPDATE '.DB_PREFIX.'ixlab_adminer SET expire_time='.(time()+600).' WHERE token="'.$_COOKIE['adminer_token'].'"');
+        setcookie('adminer_token', $_COOKIE['adminer_token'], time()+600, '/');
+        $auth = true;
     }
 }
